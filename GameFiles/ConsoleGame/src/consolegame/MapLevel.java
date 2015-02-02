7450979282b1bfce2c3ad6dc7335d81f72987a8f
PingPongLee <djda9l@gmail.com>
2015-02-02 10:47:19 +0100

    loadMap method test

--- GameFiles/ConsoleGame/src/consolegame/MapLevel.java
+++ GameFiles/ConsoleGame/src/consolegame/MapLevel.java
@@ -6,7 +6,10 @@
 package consolegame;
 
 import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -17,9 +20,27 @@ public class MapLevel {
     protected String map;
     protected List<NPC> npcs;
     
-    public void loadMap(String filename)
+    public void loadMap(String fileName)
     {
-        
+        String applicationDir = getClass().getProtectionDomain().getCodeSource().getLocation().getPath(); 
+        String fileToLoad = applicationDir + "/levels/" + fileName + ".txt";        
+        File file = new File(fileToLoad);       
+        String theLevel = "";
+        try 
+        {
+            Scanner sc = new Scanner(file);
+            
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                theLevel += line;
+            }
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }                
+        map = theLevel;
     }
     
     public String getMap()
