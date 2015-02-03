ed3282f39166ec4665b20958ee214847b458ebb5
ReneNNielsen <renennielsen@gmail.com>
2015-02-03 13:40:15 +0100

    combat Of the year

--- GameFiles/ConsoleGame/src/consolegame/Combat.java
+++ GameFiles/ConsoleGame/src/consolegame/Combat.java
@@ -5,6 +5,10 @@
  */
 package consolegame;
 
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Bruger
@@ -22,46 +26,113 @@ public class Combat {
         playerHealthRemaining = player.health;
         this.npc = npc;
         npcHealthRemaining = npc.health;
-        
     }
     
     public boolean doCombat()
     {
-        Game.clearConsole();
-        showStatsAndSkills();
-        return true;
+        boolean isPlayerTurn = true;
+        while (true) 
+        {            
+            try 
+            {
+                //Game.clearConsole();
+                showStatsAndSkills(isPlayerTurn);
+                if (isPlayerTurn)
+                {
+                    doPlayerTurn();
+                }
+                else
+                {
+                    doNpcTurn();
+                }
+                Thread.sleep(1000);
+                isPlayerTurn = !isPlayerTurn;
+                if (npcHealthRemaining <= 0)
+                {
+                    return true;
+                }
+                else if (playerHealthRemaining <= 0)
+                {
+                    return false;
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Combat.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    private void showStatsAndSkills()
+    private void showStatsAndSkills(boolean isPlayerTurn)
     {
+        System.out.println("--------------------------------------------");
         System.out.println(player.getName() + "\t\t\t" + npc.getName());
         System.out.println("Level: " + player.getLevel() + " xp: " + player.getXp() + "\t\tLevel: " + npc.getLevel());
         System.out.println("Health: " + playerHealthRemaining + "/" + player.health + "\t\tHealth: " + npcHealthRemaining + "/" + npc.health);
-        System.out.println("--------------------------------------------");
+        System.out.println("---------------------------");
         
-        System.out.println("Skils:");
-        System.out.println("------");
-        int numberToPress = 1;
-        for (Skill skill : player.getSkills()) {
-            System.out.println(numberToPress + ": " + skill.getName());
-            numberToPress++;
+        if (isPlayerTurn) 
+        {
+            System.out.println("Skils:");
+            System.out.println("------");
+            int numberToPress = 1;
+            for (Skill skill : player.getSkills()) 
+            {
+                System.out.println(numberToPress + ": " + skill.getName());
+                numberToPress++;
+            }   
         }
     }
     
     private void doNpcTurn()
     {
-        
+        int randomNumber = new Random().nextInt(npc.skills.size());
+        if (didAtackMiss(randomNumber)) 
+        {
+            System.out.println(npc.name + " did miss with " + npc.skills.get(randomNumber).name + " when trying to damage " + player.name);
+        }
+        else
+        {
+            playerHealthRemaining -= npc.skills.get(randomNumber).getDamage();
+            System.out.println(npc.name + " did " + npc.skills.get(randomNumber).getDamage() + " with " + npc.skills.get(randomNumber).name + " to " + player.name);
+        }
+    
     }
 
     private void doPlayerTurn()
     {
         try {
             String readLine = Game.br.readLine();
-            int skillNumber = Integer.parseInt(readLine);
+            int skillNumber = Integer.parseInt(readLine) - 1;
             if (skillNumber < 4) {
-                npcHealthRemaining -= player.skills.get(skillNumber).getDamage();
-                System.out.println(player.name + " did " + player.skills.get(skillNumber).getDamage() + " with " + player.skills.get(skillNumber).name + " to " + npc.name);
+                if (didAtackMiss(skillNumber)) 
+                {
+                    System.out.println(player.name + " did miss with " + player.skills.get(skillNumber).name + " when trying to damage " + npc.name);
+                }
+                else
+                {
+                    double test = player.skills.get(skillNumber).getDamage();
+                    test = Math.round(player.skills.get(skillNumber).getDamage());
+                    int damageDone = (int)Math.round(player.skills.get(skillNumber).getDamage());
+                    npcHealthRemaining -= damageDone;
+                    System.out.println(player.name + " did " + damageDone + " with " + player.skills.get(skillNumber).name + " to " + npc.name);
+                }
             }
-        } catch (Exception e) {
+            else
+            {
+                System.out.println("you dont have an skill at that number, try another");
+                doPlayerTurn();
+            }
+        } catch (Exception e) 
+        {
+            System.out.println("thats not a number try another number");
+                doPlayerTurn();
         }
+    }
+    private boolean didAtackMiss(int missChance)
+    {
+        int randomNumber = new Random().nextInt(100);
+        if (randomNumber <= missChance) 
+        {
+            return true;
+        }
+        return false;
     }
 }
